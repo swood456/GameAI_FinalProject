3f0b730b7e2ba6c430637ae8f543f3ccdb61fbc0
swood456 <crazedsportsfan@gmail.com>
2017-11-20 13:11:15 -0500

    added a simple moving snake that always goes in a line, but it moves in a way that allows us to easily speed it up for AI

---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = transform.position;
+        v.x += 1.0f;
+        transform.position = v;
+        //transform.position += 50;
+	}
+}
