34079ed8728ce6db17f11204cfe5d4d5a6177580
swood456 <crazedsportsfan@gmail.com>
2017-11-20 12:28:54 -0500

    started setup file directory for project

---
+++ Assets/Scripts/MakeWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeWorld : MonoBehaviour {
+
+    [SerializeField]
+    private int map_w = 10;
+
+    [SerializeField]
+    private int map_h = 10;
+
+    [SerializeField]
+    private GameObject Wall;
+
+    [SerializeField]
+    private int start_length;
+
+    [SerializeField]
+    private GameObject SnakeHead;
+
+    // Use this for initialization
+    void Start () {
+        createMap();	
+	}
+	
+	void createMap()
+    {
+
+    }
+}
