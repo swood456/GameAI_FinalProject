a29584831d00b5be06edc92a4f5599c56fa1cf00
swood456 <crazedsportsfan@gmail.com>
2017-11-25 18:22:26 -0500

    added simple create world grid logic, next step is to make the snake move around the board and die

--- Assets/Scripts/MakeWorld.cs
+++ Assets/Scripts/MakeWorld.cs
@@ -19,13 +19,33 @@ public class MakeWorld : MonoBehaviour {
     [SerializeField]
     private GameObject SnakeHead;
 
+    private char[,] map;
+
     // Use this for initialization
     void Start () {
-        createMap();	
+        createMap();
 	}
 	
 	void createMap()
     {
+        // for now map is a 2d array of chars, might be better to be something simpler?
+        map = new char[map_w + 2, map_h + 2];
 
+        for(int i = 0; i < map_w + 2; ++i)
+        {
+            map[i, 0] = 'w';
+            map[i, map_h + 1] = 'w';
+        }
+
+        for(int j = 0; j < map_h; ++j)
+        {
+            map[0, j + 1] = 'w';
+            map[map_w + 1, j + 1] = 'w';
+        }
+
+        int s_i = Random.Range(0, map_w - 1);
+        int s_j = Random.Range(0, map_h - 1);
+
+        map[s_i + 1, s_j + 1] = 'h';
     }
 }
